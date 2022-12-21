30c7e6a81a6e8c4b38a17d7eac180273bb820f76
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2022-12-21 20:38:16 -0300

    commit 9

--- src/Components/ImageFilter.js
+++ src/Components/ImageFilter.js
@@ -3,15 +3,15 @@ import alimentacao from "../assets/images/alimentacao.svg";
 import outros from "../assets/images/outros.svg";
 import saude from "../assets/images/saude.svg";
 import transporte from "../assets/images/transporte.svg";
-import utilidades from "../assets/images/utilidades.svg";import { Icone } from "./UI";
+import utilidades from "../assets/images/utilidades.svg";import { IconeTema } from "./UI";
 
 export default (type) => {
   const Images = {
-    Restaurante: <Icone src={alimentacao} alt="Restaurante" />,
-    Saude: <Icone src={saude} alt="Saude" />,
-    Transporte: <Icone src={transporte} alt="Transporte" />,
-    Utilidades: <Icone src={utilidades} alt="Utilidades" />,
-    default: <Icone src={outros} alt="Outros" />,
+    Restaurante: <IconeTema src={alimentacao} alt="Restaurante" />,
+    Saude: <IconeTema src={saude} alt="Saude" />,
+    Transporte: <IconeTema src={transporte} alt="Transporte" />,
+    Utilidades: <IconeTema src={utilidades} alt="Utilidades" />,
+    default: <IconeTema src={outros} alt="Outros" />,
   };
 
   return Images[type] || Images.default;
